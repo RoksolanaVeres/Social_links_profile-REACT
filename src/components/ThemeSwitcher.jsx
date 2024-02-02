43202b00f19a39e1f0223f882850e369b389c7fd
Roksolana Veres <Roxolanka1@ukr.net>
2024-02-02 19:56:36 +0200

    attempted to save theme in Local storage

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -3,33 +3,12 @@ import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
 export default function ThemeSwitcher() {
-  const { themeValue, setThemeValue } = useContext(ThemeContext);
-  console.log(themeValue);
-
-  // const [theme, setTheme] = useState(() => {
-  //   const initialTheme = localStorage.getItem("theme");
-  //   return initialTheme ? initialTheme : "dark";
-  // });
-
-  // function getThemeFromLocalStorage() {
-  //   const savedTheme = localStorage.getItem("theme");
-  //   savedTheme && setTheme(savedTheme);
-  //   savedTheme === "light" && document.documentElement.classList.remove("dark");
-  // }
+  const { theme, setTheme } = useContext(ThemeContext);
+  console.log(theme);
 
   function toggleTheme() {
-    setThemeValue((prevTheme) => {
-      const newTheme = prevTheme === "dark" ? "light" : "dark";
-      localStorage.setItem("theme", newTheme);
-      return newTheme;
-    });
-
-    document.documentElement.classList.toggle("dark");
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
   }
-
-  // useEffect(() => {
-  //   getThemeFromLocalStorage();
-  // }, [theme]);
 
   return (
     <div className="self-end text-white">
@@ -37,7 +16,7 @@ export default function ThemeSwitcher() {
         className="flex size-[40px] items-center justify-center rounded-xl hover:bg-slate-200 hover:dark:bg-darkGrey"
         onClick={toggleTheme}
       >
-        {themeValue === "dark" ? <SunIcon /> : <MoonIcon />}
+        {theme === "dark" ? <SunIcon /> : <MoonIcon />}
       </button>
     </div>
   );
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,11 +1,37 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
-export const ThemeContext = createContext({ themeValue: "dark" });
+export const ThemeContext = createContext(null);
 
-export const ThemeContextProvider = ({ children, initial = "dark" }) => {
-  const [themeValue, setThemeValue] = useState(initial);
+export const ThemeContextProvider = ({
+  children,
+  defaultTheme = "dark",
+  storageKey = "app-theme",
+  ...props
+}) => {
+  const [theme, setTheme] = useState(
+    () => localStorage.getItem(storageKey) || defaultTheme,
+  );
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (theme === "dark") {
+      root.classList.add("dark");
+    } else {
+      root.classList.remove("dark");
+    }
+  }, [theme]);
+
+  function setThemeInLocalStorage(theme) {
+    localStorage.setItem(storageKey, theme);
+    setTheme(theme);
+  }
+
+  const value = {
+    theme,
+    setTheme: setThemeInLocalStorage,
+  };
   return (
-    <ThemeContext.Provider value={{ themeValue, setThemeValue }}>
+    <ThemeContext.Provider {...props} value={value}>
       {children}
     </ThemeContext.Provider>
   );
